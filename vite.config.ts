c624e6e5f0b09bb96e2e4ab7b09448a9cada15c8
Lucas Arundell <lucas.arundell@magentus.com>
2025-11-18 22:12:40 +1000

    Configure dynamic base path for dev and production environments

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './',
+  base: command === 'build' ? '/shadality/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
